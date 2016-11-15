a6f3f9f9d932dee03c7f230eb326b42edc931b30
jungtaejtkim <jungtae.jt.kim@gmail.com>
2016-11-15 13:56:42 +0900

    create show action and add gravatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,11 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def show
+		@user = User.find(params[:id])
+	end
+	
+
 
 
 	private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def gravatar_for(user, options = { size: 80})
+	  gravatar_id = Digest::MD5::hexdigest(user.email.downcase) # gravatar 에서 제공하는 md5 기반의 해쉬 
+	  size = options[:size]
+	  gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}" 
+	  image_tag(gravatar_url, alt:user.username, class: "img-circle")  # << 부트스트랩이 제공하는 클래스    	
+	end
+
 end
